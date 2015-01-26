f55964f76733a4e394b3bc82915cd1e68a94f2e2
Matthieu <matt.johnston@uvm.edu>
2015-01-26 09:05:29 -0500

    Added comments

--- TrianglePegArray/src/trianglepegarray/TrianglePegArray.java
+++ TrianglePegArray/src/trianglepegarray/TrianglePegArray.java
@@ -1,31 +1,46 @@
 
 //package trianglepegarray;
-
 public class TrianglePegArray {
 
+    //Declares an 2D array to store x's and o's
     private final char[][] Triangle = {{'x'}, {'x', 'x'}, {'o', 'x', 'x'}, {'x', 'x', 'x', 'x'}, {'x', 'x', 'x', 'x', 'x'}};
-    
-    public void createNodes(int x, int y){
-    
+
+    /*create nodes from the Node class to store in the indexes
+     *
+     */
+    public void createNodes(int x, int y) {
+
         for (int i = 0; i < Triangle.length; i++) {
             for (int j = 0; j < Triangle.length; j++) {
-                Triangle[i][j] = new Node(x,y);
+                Triangle[i][j] = new Node(x, y);
             }
+        }
     }
-    }
-    public int getIndex(int i, int j) { return Triangle[i][j]; }
 
-    public void changeIndex(int i, int j){
-        if(Triangle[i][j] == 'x'){
+    /*
+     * Returns given peg at the current index
+     */
+    public int getIndex(int i, int j) {
+        return Triangle[i][j];
+    }
+
+    /*
+     * Changes the index (needs to be changed)
+     */
+    public void changeIndex(int i, int j) {
+        if (Triangle[i][j] == 'x') {
             Triangle[i][j] = 'o';
-        }else{
+        } else {
             Triangle[i][j] = 'x';
         }
         print();
     }
 
-    public void print(){
-           for (char[] Triangle1 : Triangle) {
+    /*
+     * Prints out the array 
+     */
+    public void print() {
+        for (char[] Triangle1 : Triangle) {
             for (int j = 0; j < Triangle1.length; j++) {
                 System.out.print(Triangle1[j] + " ");
             }
