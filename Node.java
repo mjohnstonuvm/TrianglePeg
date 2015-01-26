5ff30cd2ce9ef9d64ebc066cf65cdc404b65fed4
colin luther <cluther@uvm.edu>
2015-01-26 15:14:53 -0500

    Some HashMap Updates
    I incorporated a HashMap for the Node neighbors. All the functionality
    isn’t done yet but it will be soon (no getNeighbor or setNeighbor yet).
    
    Also, changed constructor from coordinates to label, and included
    setter and getter for label variable.

--- Node.java
+++ Node.java
@@ -1,9 +1,12 @@
+import java.util.*;
+
 /*
    Each node represents a hole on the game board. There are eight
    possible positions that a neighboring node could be. A node's 
-   coordinates determine where on the game board it is located.
+   label determine where on the game board it is located.
    The peg variable represents whether a node(hole) has a peg in it.
 */
+
 public class Node 
 {
    /*
@@ -11,121 +14,62 @@ public class Node
    */
    private boolean peg = true;
    
-   private int xCoordinate;
-   private int yCoordinate;
-  
-   //Variable names correspond to cardinal directions.
-   private Node nNeighbor = null;
-   private Node nENeighbor = null;
-   private Node eNeighbor = null;
-   private Node sENeighbor = null;
-   private Node sNeighbor = null;
-   private Node sWNeighbor = null;
-   private Node wNeighbor = null;
-   private Node nWNeighbor = null;
+   private int label;
+         
+   private HashMap neighbors = new HashMap();
    
    /*
-      Node constructor must accept X and Y coordinates in integer form.
+      Node constructor must accept label in integer form.
    */
-   public Node(int xCoordinate, int yCoordinate) {
-      this.xCoordinate = xCoordinate;
-      this.yCoordinate = yCoordinate;
+   public Node(int label) 
+   {
+      this.label = label;
+      
+      neighbors = startHashMap();
    } 
     
    /*
       Setter and getter for "peg" variable.
    */ 
-   public void setPeg(boolean peg) {
+   public void setPeg(boolean peg) 
+   {
       this.peg = peg;
    } 
 
-   public boolean getPeg() {
+   public boolean getPeg() 
+   {
       return peg;
    } 
    
    /*
-      Setters and getters for "coordinate" variables. (Try not to use the setters.)
+      Setter and getter for label variable.
    */
-   public void setXCoordinate(int xCoordinate) {
-      this.xCoordinate = xCoordinate;
+   public void setLabel(int label) 
+   {
+      this.label = label;
    } 
 
-   public int getXCoordinate() {
-      return xCoordinate;
+   public int getLabel() 
+   {
+      return label;
    }
-   
-   public void setYCoordinate(int yCoordinate) {
-      this.yCoordinate = yCoordinate;
-   } 
-
-   public int getYCoordinate() {
-      return yCoordinate;
-   }
-
-
+      
    /*
-      Setters and getters for "neighbor" variables.
+      startHashMap initializes all 8 possible neighbors of a node(hole) to be null.
+      This should only be called when Node is first constructed.
    */
-   //north
-   public void setNNeighbor(Node nNeighbor) {
-      this.nNeighbor = nNeighbor;
+   
+   private HashMap startHashMap()
+   {
+      neighbors.put("n", null);
+      neighbors.put("ne", null);
+      neighbors.put("e", null);
+      neighbors.put("se", null);
+      neighbors.put("s", null);
+      neighbors.put("sw", null);
+      neighbors.put("w", null);
+      neighbors.put("nw", null);
+ 
+      return neighbors;
    } 
-
-   public Node getNNeighbor() {
-      return nNeighbor;
-   }
-   //northeast
-   public void setNENeighbor(Node nENeighbor) {
-      this.nENeighbor = nENeighbor;
-   } 
-
-   public Node getNENeighbor() {
-      return nENeighbor;
-   }
-   //east
-   public void setENeighbor(Node eNeighbor) {
-      this.eNeighbor = eNeighbor;
-   } 
-
-   public Node getENeighbor() {
-      return eNeighbor;
-   }
-   //southeast
-   public void setSENeighbor(Node sENeighbor) {
-      this.sENeighbor = sENeighbor;
-   } 
-
-   public Node getSENeighbor() {
-      return sENeighbor;
-   }
-   //south
-   public void setSNeighbor(Node SNeighbor) {
-      this.sNeighbor = sNeighbor;
-   } 
-   public Node getSNeighbor() {
-      return sNeighbor;
-   }
-   //southwest
-   public void setSWNeighbor(Node sWNeighbor) {
-      this.sWNeighbor = sWNeighbor;
-   } 
-   public Node getSWNeighbor() {
-      return sWNeighbor;
-   }
-   //west
-   public void setWNeighbor(Node wNeighbor) {
-      this.wNeighbor = wNeighbor;
-   } 
-   public Node getWNeighbor() {
-      return wNeighbor;
-   }
-   //northwest
-   public void setNWNeighbor(Node nWNeighbor) {
-      this.nWNeighbor = nWNeighbor;
-   } 
-
-   public Node getNWNeighbor() {
-      return nWNeighbor;
-   }
-
-} 
+}
