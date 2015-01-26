85c3373b698c97297882d472df2a162036b18e67
Matthieu <matt.johnston@uvm.edu>
2015-01-26 08:31:01 -0500

    Created data structure

---
+++ TrianglePegArray.java
@@ -0,0 +1,35 @@
+
+package trianglepegarray;
+
+public class TrianglePegArray {
+
+    private final char[][] Triangle = {{'x'}, {'x', 'x'}, {'o', 'x', 'x'}, {'x', 'x', 'x', 'x'}, {'x', 'x', 'x', 'x', 'x'}};
+    
+    public void createNodes(int x, int y){
+    
+        for (int i = 0; i < Triangle.length; i++) {
+            for (int j = 0; j < Triangle.length; j++) {
+                Triangle[i][j] = new Node(x,y);
+            }
+    }
+    }
+    public int getIndex(int i, int j) { return Triangle[i][j]; }
+
+    public void changeIndex(int i, int j){
+        if(Triangle[i][j] == 'x'){
+            Triangle[i][j] = 'o';
+        }else{
+            Triangle[i][j] = 'x';
+        }
+        print();
+    }
+
+    public void print(){
+           for (char[] Triangle1 : Triangle) {
+            for (int j = 0; j < Triangle1.length; j++) {
+                System.out.print(Triangle1[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+};
