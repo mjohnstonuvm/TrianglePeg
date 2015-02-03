12d19e332b2134294eb1669a8e51cc69d4676682
colin luther <cluther@uvm.edu>
2015-02-02 23:19:08 -0500

    A lot of shit.
    Made changes to almost every file.
    Triangle board works now.
    Cross has fence-post error somewhere.
    Maybe check that out Sam or Matt?
    Haven’t figured out reset function yet.
    Included first paragraph of individual report.
    Let me know if you can’t open it and I’ll replied in a different format.
    
    I’m going to bed. Goodnight.

--- Node.java
+++ Node.java
@@ -1,3 +1,4 @@
+//(Colin)
 import java.util.*;
 
 /*
@@ -42,7 +43,7 @@ public class Node
       neighbors.put("w", null);
       neighbors.put("nw", null);
    } 
-    
+       
    /*
       Setter and getter for "peg" variable.
    */ 
---
+++ TrianglePegArray.java
@@ -0,0 +1,130 @@
+//(Matt)
+public class TrianglePegArray {
+
+    //Declares an 2D array
+    public Node[][] Triangle = new Node[5][];
+
+    /*create nodes from the Node class to store in the indexes
+     *
+     */
+    public TrianglePegArray() {
+
+        //Creates node number per index
+        Triangle[0] = new Node[1];
+        Triangle[1] = new Node[2];
+        Triangle[2] = new Node[3];
+        Triangle[3] = new Node[4];
+        Triangle[4] = new Node[5];
+
+        int num = 1; //counter
+
+        //loop to create the nodes with a number
+        for (int i = 0; i < Triangle.length; i++) {
+            for (int j = 0; j < Triangle[i].length; j++) {
+                Triangle[i][j] = new Node(num);
+                num++;
+            }
+
+        }
+
+        //Calls method
+        createNeighbors();
+    }
+
+    /*
+     * Creates neighbors
+     */
+    private void createNeighbors() {
+
+        //loop to create sw neighbors with index i
+        for (int i = 0; i < Triangle.length; i++) {
+
+            for (int j = 0; j < Triangle[i].length; j++) {
+
+                if (i + 1 <= 4) {
+
+                    //sets the neighbor
+                    Triangle[i][j].setNeighbor("sw", Triangle[i + 1][j]);
+
+                }
+
+            }
+        }
+
+        //loop to create ne neighbors
+        for (int i = Triangle.length - 1; i > 0; i--) {
+
+            for (int j = 0; j < i; j++) {
+
+                //sets the neighbor
+                Triangle[i][j].setNeighbor("ne", Triangle[i - 1][j]);
+
+            }
+
+        }
+
+        //loop to create nw neighbors
+        for (int i = Triangle.length - 1; i > 0; i--) {
+
+            for (int j = 0; j <= i; j++) {
+
+                if (j >= 1) {
+
+                    //sets the neighbor
+                    Triangle[i][j].setNeighbor("nw", Triangle[i - 1][j - 1]);
+
+                }
+
+            }
+
+        }
+
+        //loop to create se seighbors same as nw but flipping indexes
+        for (int i = Triangle.length - 1; i >= 0; i--) {
+
+            for (int j = 0; j <= i; j++) {
+
+                if (i < 4) {
+
+                    //sets the neighbor
+                    Triangle[i][j].setNeighbor("se", Triangle[i + 1][j + 1]);
+
+                }
+
+            }
+
+        }
+
+        //loop to create e neighbors
+        for (int i = 0; i < Triangle.length; i++) {
+
+            for (int j = 0; j < Triangle[i].length; j++) {
+
+                //if the length -1 is not 0, then add the neighbor
+                if (Triangle[j].length - 1 > 0) {
+
+                    //sets the neighbor
+                    Triangle[i][j - 1].setNeighbor("e", Triangle[i][j]);
+                }
+            }
+
+        }
+
+        //loop to create w neighbors
+        for (int i = 0; i < Triangle.length; i++) {
+
+            for (int j = 0; j < Triangle[i].length; j++) {
+
+                //if the length -1 is not 0, then add the neighbor
+                if (Triangle[j].length - 1 > 0) {
+
+                    //sets the neighbor
+                    Triangle[i][j].setNeighbor("w", Triangle[i][j - 1]);
+                }
+            }
+
+        }
+    }
+
+
+};
