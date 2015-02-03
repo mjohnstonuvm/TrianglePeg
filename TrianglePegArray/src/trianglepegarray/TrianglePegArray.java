68e04c36ca1d6f66516541cb0518b7f7f7497cbf
mjohnstonuvm <Matt.Johnston@uvm.edu>
2015-02-02 22:27:14 -0500

    fix

--- TrianglePegArray/src/trianglepegarray/TrianglePegArray.java
+++ TrianglePegArray/src/trianglepegarray/TrianglePegArray.java
@@ -1,5 +1,4 @@
 
-
 public class TrianglePegArray {
 
     //Declares an 2D array
@@ -40,9 +39,9 @@ public class TrianglePegArray {
         //loop to create sw neighbors with index i
         for (int i = 0; i < Triangle.length; i++) {
 
-            for (int j = i; j < Triangle[i].length; j++) {
+            for (int j = 0; j < Triangle[i].length; j++) {
 
-                if (i + 1 < Triangle.length) {
+                if (i + 1 <= 4) {
 
                     //sets the neighbor
                     Triangle[i][j].setNeighbor("sw", Triangle[i + 1][j]);
@@ -52,66 +51,27 @@ public class TrianglePegArray {
             }
         }
 
-        int colsNorthEast = 0; //counter or cols
-        int rowsNorthEast = 0; //counter for rows
-
         //loop to create ne neighbors
         for (int i = Triangle.length - 1; i > 0; i--) {
 
             for (int j = 0; j < i; j++) {
 
                 //sets the neighbor
-                Triangle[j + 1][j].setNeighbor("ne", Triangle[rowsNorthEast][colsNorthEast]);
-
-                //incrememnts
-                rowsNorthEast++;
-
-                if (j == i - 1) {
-                    colsNorthEast++;
-                    rowsNorthEast = colsNorthEast;
-                }
+                Triangle[i][j].setNeighbor("ne", Triangle[i - 1][j]);
 
             }
 
         }
-
-        int colsNorthWest = 0; //counter for cols for the neighbor
-        int rowsNorthWest = 0; //counter for rows for the neighbor
-        int colsNeighbor = 1; //counter for cols for the current index
-        int rowsNeighbor = 1; //counter for rows for the current index
-        int count = 0; //counter
 
         //loop to create nw neighbors
         for (int i = Triangle.length - 1; i > 0; i--) {
 
-            for (int j = 0; j < i; j++) {
+            for (int j = 0; j <= i; j++) {
 
-                //sets the neighbor
-                Triangle[rowsNeighbor][colsNeighbor].setNeighbor("nw", Triangle[rowsNorthWest][colsNorthWest]);
+                if (j >= 1) {
 
-                //increments
-                rowsNeighbor++;
-                rowsNorthWest++;
-                colsNeighbor++;
-                colsNorthWest++;
-
-                if (j == i - 1) {
-
-                    count++;
-                    rowsNeighbor = 1;
-                    colsNorthWest = 0;
-                    colsNeighbor = 1;
-                    rowsNorthWest = 1;
-
-                    if (count == 2) {
-
-                        rowsNorthWest = 2;
-
-                    }
-                    if (count == 3) {
-
-                        rowsNorthWest = 3;
-                    }
+                    //sets the neighbor
+                    Triangle[i][j].setNeighbor("nw", Triangle[i - 1][j - 1]);
 
                 }
 
@@ -119,43 +79,15 @@ public class TrianglePegArray {
 
         }
 
-        int colsSouthEast = 0; //counter for south east cols
-        int rowsSouthEast = 0; //counter for south east rows
-        int colsNeighborSE = 1; //counter for south east neighbor cols
-        int rowsNeighborSE = 1; //counter for south east neighbor rows
-        int countSE = 0;
-
         //loop to create se seighbors same as nw but flipping indexes
-        for (int i = Triangle.length - 1; i > 0; i--) {
+        for (int i = Triangle.length - 1; i >= 0; i--) {
 
-            for (int j = 0; j < i; j++) {
+            for (int j = 0; j <= i; j++) {
 
-                //sets the neighbor
-                Triangle[rowsSouthEast][colsSouthEast].setNeighbor("se", Triangle[rowsNeighborSE][colsNeighborSE]);
+                if (i < 4) {
 
-                //increments
-                rowsNeighborSE++;
-                rowsSouthEast++;
-                colsNeighborSE++;
-                colsSouthEast++;
-
-                if (j == i - 1) {
-
-                    countSE++;
-                    rowsNeighborSE = 1;
-                    colsSouthEast = 0;
-                    colsNeighborSE = 1;
-                    rowsSouthEast = 1;
-
-                    if (countSE == 2) {
-
-                        rowsSouthEast = 2;
-
-                    }
-                    if (countSE == 3) {
-
-                        rowsSouthEast = 3;
-                    }
+                    //sets the neighbor
+                    Triangle[i][j].setNeighbor("se", Triangle[i + 1][j + 1]);
 
                 }
 
@@ -168,11 +100,11 @@ public class TrianglePegArray {
 
             for (int j = 0; j < Triangle[i].length; j++) {
 
-                //if the length -2 is not 0, then add the neighbor
-                if (Triangle[j].length - 2 > 0) {
+                //if the length -1 is not 0, then add the neighbor
+                if (Triangle[j].length - 1 > 0) {
 
                     //sets the neighbor
-                    Triangle[i][j - 2].setNeighbor("e", Triangle[i][j]);
+                    Triangle[i][j - 1].setNeighbor("e", Triangle[i][j]);
                 }
             }
 
@@ -183,16 +115,16 @@ public class TrianglePegArray {
 
             for (int j = 0; j < Triangle[i].length; j++) {
 
-                //if the length -2 is not 0, then add the neighbor
-                if (Triangle[j].length - 2 > 0) {
+                //if the length -1 is not 0, then add the neighbor
+                if (Triangle[j].length - 1 > 0) {
 
                     //sets the neighbor
-                    Triangle[i][j].setNeighbor("w", Triangle[i][j - 2]);
+                    Triangle[i][j].setNeighbor("w", Triangle[i][j - 1]);
                 }
             }
 
         }
-
     }
+
 
 };
